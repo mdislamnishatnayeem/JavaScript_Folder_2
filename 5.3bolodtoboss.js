5f5dee4c725e3719208b337f6b8510bcb8a00764
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-09-11 00:45:51 +0600

    Update 5.3bolodtoboss.js

--- 5.3bolodtoboss.js
+++ 5.3bolodtoboss.js
@@ -16,3 +16,22 @@ function  greatestCommonDivisor(a,b){
 
 var v=greatestCommonDivisor(66,55);
 console.log(v);
+
+
+//আমি নিজে রিকার্সন পদ্ধতিতে একটি কোড দিচ্ছি।
+// এই কোডটা ঝংকার মাহবুব ভাইয়া
+// এর ওয়েবসাইট থেকে নেওয়া!!! যদিও আমি
+// নিজেও কোডটি বুঝিনি
+
+function greatestCommonDivisor(a,b){
+    if(b==0){
+ 
+        return a;
+    }
+    else{
+        return greatestCommonDivisor(b,a%b);
+   }
+}
+
+var v=greatestCommonDivisor(23,23);
+console.log(v);
